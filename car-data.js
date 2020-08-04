e55e339c9663f0a3bd272f2dea9398fa6a073796
Asher623 <69222787+Asher623@users.noreply.github.com>
2020-08-04 19:22:03 -0400

    qwe

---
+++ car-data.js
@@ -0,0 +1,101 @@
+window.carsData = [
+  {
+    model: 'Model S',
+    brand: 'Tesla',
+    category: 'Electric',
+    price: 80000,
+    image: 'images/model_s.jpg',
+    description:
+      'Model S is built from the ground up as an electric vehicle, with high-strength architecture and a floor-mounted battery pack allowing for incredible impact protection.'
+  },
+  {
+    model: 'Model X',
+    brand: 'Tesla',
+    category: 'Electric',
+    price: 85000,
+    image: 'images/model_x.jpg',
+    description:
+      'Model X is one of the safest SUVs ever. Built from the ground up as an electric vehicle, the body, chassis, restraints and battery technology provide a very low probability of occupant injury.'
+  },
+  {
+    model: 'Model 3',
+    brand: 'Tesla',
+    category: 'Electric',
+    price: 40000,
+    image: 'images/model_3.jpg',
+    description:
+      'Safety is the most important part of the overall Model 3 design. The metal structure is a combination of aluminum and steel, for maximum strength in every area.'
+  },
+  {
+    model: 'S Class',
+    brand: 'Mercedes-Benz',
+    category: 'Gas',
+    price: 100000,
+    image: 'images/s_class.jpg',
+    description:
+      'Officially unveiled in May 2013, the newest S-Class has a more streamlined appearance than the outgoing model. Some interesting features include a large front grille inspired by the F700 Concept car. '
+  },
+  {
+    model: 'C Class',
+    brand: 'Mercedes-Benz',
+    category: 'Gas',
+    price: 40000,
+    image: 'images/c_class.jpg',
+    description:
+      ' It is the first car to use the all-new Modular Rear Architecture (MRA) platform, which it shares with the contemporary Mercedes-Benz E-Class and S-Class models. The new structure is significantly lighter using aluminium.'
+  },
+  {
+    model: 'GLS Class',
+    brand: 'Mercedes-Benz',
+    category: 'Gas',
+    price: 110000,
+    image: 'images/gls_class.jpg',
+    description:
+      'The engine options at the launch are 3.0-litre turbo inline six (GLS 450 4MATIC) and 4.0-litre twin-turbo V8 (GLS 580 4MATIC), both petrol with EQ Boost electric motors.'
+  },
+  {
+    model: 'M3',
+    brand: 'BMW',
+    category: 'Gas',
+    price: 50000,
+    image: 'images/m3.jpg',
+    description:
+      'The BMW M3 is a high-performance version of the BMW 3 Series, developed by BMWs in-house motorsport division, BMW M GmbH. M3 models have been produced for every generation of 3 Series since the E30 M3 was introduced in 1986.'
+  },
+  {
+    model: 'Camry',
+    brand: 'Toyota',
+    category: 'Gas',
+    price: 30000,
+    image: 'images/camry.jpg',
+    description:
+      'Is an automobile sold internationally by the Japanese manufacturer Toyota since 1982, spanning multiple generations. Originally compact in size (narrow-body), later Camry models have grown to fit the mid-size classification (wide-body)—although the two sizes co-existed in the 1990s.'
+  },
+  {
+    model: 'Corolla',
+    brand: 'Toyota',
+    category: 'Gas',
+    price: 30000,
+    image: 'images/corolla.jpg',
+    description:
+      'is a line of subcompact and compact cars manufactured by Toyota. Introduced in 1966, the Corolla was the best-selling car worldwide by 1974 and has been one of the best-selling cars in the world since then.'
+  },
+  {
+    model: 'Taycan',
+    brand: 'Porsche',
+    category: 'Electric',
+    price: 100000,
+    image: 'images/taycan.gif',
+    description:
+      'The Porsche Taycan is an all-electric car manufactured by German automobile manufacturer Porsche, which was first unveiled as a concept car named Mission E at the 2015 Frankfurt Motor Show[4] and then in production form at the 2019 Frankfurt Motor Show.'
+  },
+  {
+    model: 'Leaf',
+    brand: 'Nissan',
+    category: 'Electric',
+    price: 40000,
+    image: 'images/leaf.jpg',
+    description:
+      'stylised by manufacturer Nissan as LEAF, is a compact five-door hatchback battery electric vehicle (BEV), introduced in Japan and the United States in December 2010, and now in its second generation, introduced in Japan in October 2017.'
+  }
+];
---
+++ script.js
@@ -0,0 +1,142 @@
+/* Add any JavaScript you need to this file. */
+
+(function() {
+  let cars = {
+    all: window.carsData,
+    getByCategory: function(category) {
+      return window.carsData.filter(car => {
+        if (car.category === category) {
+          return Object.assign({}, car);
+        }
+      });
+    },
+
+    getByBrand: function(brand) {
+      return window.carsData.filter(car => {
+        if (car.brand === brand) {
+          return Object.assign({}, car);
+        }
+      });
+    }
+  };
+
+  let tableHelper = {
+    clearTable: function() {
+      let body = document.querySelector('#table-rows');
+      body.innerHTML = '';
+    },
+
+    carToRow: function(car) {
+      let tr = document.createElement('tr');
+
+      let tdImage = document.createElement('td');
+      let img = document.createElement('img');
+      img.src = car.image;
+      img.onload = function() {
+        console.log('Loaded');
+      };
+      img.onerror = function() {
+        this.remove();
+      };
+      img.style.width = '375px';
+      img.style.height = '230px';
+      tdImage.appendChild(img);
+      tr.appendChild(tdImage);
+
+      let trBrand = document.createElement('tr');
+      let brandH2 = document.createElement('h2');
+      let textBrand = document.createTextNode(car.brand);
+      brandH2.appendChild(textBrand);
+      trBrand.appendChild(brandH2);
+      tr.appendChild(trBrand);
+
+      let trModel = document.createElement('tr');
+      let modelH2 = document.createElement('p');
+      let textModel = document.createTextNode(car.model + ' ' + car.category);
+      modelH2.setAttribute('id', 'model');
+      modelH2.appendChild(textModel);
+      trModel.appendChild(modelH2);
+      tr.appendChild(trModel);
+
+      let trPrice = document.createElement('tr');
+      let textPrice = document.createTextNode('Price: ' + car.price + ' $CAD');
+      let priceP = document.createElement('p');
+      priceP.setAttribute('id', 'price');
+      priceP.appendChild(textPrice);
+      trPrice.appendChild(priceP);
+      tr.appendChild(trPrice);
+
+      let trDesc = document.createElement('tr');
+      let textDesc = document.createTextNode(car.description);
+      let descP = document.createElement('p');
+      descP.setAttribute('id', 'text');
+      descP.appendChild(textDesc);
+      trDesc.appendChild(descP);
+      tr.appendChild(trDesc);
+
+      let body = document.querySelector('#table-rows');
+      body.appendChild(tr);
+    },
+
+    carToTable: function(cars) {
+      tableHelper.clearTable();
+      var car;
+      for (car of cars) {
+        tableHelper.carToRow(car);
+      }
+    }
+  };
+
+  function setupMenuHandlers() {
+    tableHelper.carToTable(cars.all);
+    let menuElectric = document.querySelector('#electric_category');
+    menuElectric.addEventListener('click', function() {
+      tableHelper.carToTable(cars.getByCategory('Electric'));
+    });
+    let menuGas = document.querySelector('#gas_category');
+    menuGas.addEventListener('click', function() {
+      tableHelper.carToTable(cars.getByCategory('Gas'));
+    });
+    let menuTesla = document.querySelector('#Tesla');
+    menuTesla.addEventListener('click', function() {
+      tableHelper.carToTable(cars.getByBrand('Tesla'));
+    });
+
+    let menuNissan = document.querySelector('#Nissan');
+    menuNissan.addEventListener('click', function() {
+      tableHelper.carToTable(cars.getByBrand('Nissan'));
+    });
+
+    let menuMercedes = document.querySelector('#Mercedes-Benz');
+    menuMercedes.addEventListener('click', function() {
+      tableHelper.carToTable(cars.getByBrand('Mercedes-Benz'));
+    });
+
+    let menuPorsche = document.querySelector('#Porsche');
+    menuPorsche.addEventListener('click', function() {
+      tableHelper.carToTable(cars.getByBrand('Porsche'));
+    });
+    let menuToyota = document.querySelector('#Toyota');
+    menuToyota.addEventListener('click', function() {
+      tableHelper.carToTable(cars.getByBrand('Toyota'));
+    });
+    let menuBMW = document.querySelector('#BMW');
+    menuBMW.addEventListener('click', function() {
+      tableHelper.carToTable(cars.getByBrand('BMW'));
+    });
+    let menuAll = document.querySelector('#logo-cars');
+    menuAll.addEventListener('click', function() {
+      tableHelper.carToTable(cars.all);
+    });
+  }
+
+  let table = document.querySelector('#table-rows');
+  let m = cars.getByBrand('Mercedes-Benz');
+  let node = document.createTextNode(m.model);
+
+  let pp = document.createElement('p');
+  pp.appendChild(node);
+  table.appendChild(node);
+
+  window.onload = setupMenuHandlers;
+})();
